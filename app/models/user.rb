9d8e14be44898c35daf828087e63a5d778a56f75
AlexTruck <alexandr.kujba@gmail.com>
2015-10-07 11:14:46 +0300

    add name param to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :registerable, :authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151006144437) do
+ActiveRecord::Schema.define(:version => 20151007070215) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20151006144437) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
